15a12ae8467f4f0a2ce74ef1e2acba4a3dd41a60
Liwer <liwermon@gmail.com>
2015-02-03 20:35:27 +0200

    fixed error

--- db/migrate/20150203180920_create_microposts.rb
+++ db/migrate/20150203180920_create_microposts.rb
@@ -1,7 +1,7 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
-      t.srting :content
+      t.string :content
       t.integer :user_id
 
       t.timestamps null: false
